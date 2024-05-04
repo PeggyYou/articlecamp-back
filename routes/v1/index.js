21569bf92d2efd6759a7d0c4f5c57b208cc2b3cb
Peggy <peggyyu82@gmail.com>
2024-05-04 16:57:26 +0800

    feat: 路由新增 v1 及 v2

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -4,9 +4,9 @@ const  { Router }  = require('express')
 // 建立路由
 const router = Router()
 
-// API-v1
-const v1 = require('./v1')
-router.use('/v1', v1)
+// articles
+const articles = require('./articles')
+router.use('/articles', articles)
 
 // 匯出 router
 module.exports = router
